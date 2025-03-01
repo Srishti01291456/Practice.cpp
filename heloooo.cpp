46ec586a3dba459b67db49b234e6a16e3af61224
24csu205 <24csu205@ncuindia.edu>
2025-03-02 00:18:09 +0530

    Add files via upload

---
+++ heloooo.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string name;
+    int age;
+    cin>>name;
+    cin>>age;
+    cout<<"Hello"<<name<<"you are"<<age<<"years old!"<<endl;
+    return 0;
+}
